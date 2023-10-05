e1820fa2105147c3f62172578ea43143651e4d81
Pavel Davydov <Davydov.works@gmail.com>
2023-10-05 11:01:00 -0400

    create test + change bild.

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,11 +1,33 @@
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+
+
 public class CardTest {
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
     @Test
-    void shouldOpenTest(){
-        open ("http://0.0.0.0:9999");
+    void shouldOpenTest() throws InterruptedException{
+        open("http://0.0.0.0:9999");
+        $("[data-test-id=city] input").setValue("Казань");
+        String planDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id=date] input").setValue(planDate);
+        $("[data-test-id=name] input").setValue("Петр И");
+        $("[data-test-id=phone] input").setValue("+79600000000");
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(13))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + planDate));
+
     }
 }
